2ed0b8fb73c1e906797bbf1084ca4fcbe7382f10
Jeplind Morico <jeplindmorico@gmail.com>
2022-04-04 17:24:09 +0700

    Added font awesome icon

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -1,9 +1,20 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import './index.css'
+import "./index.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add({
+  faUserSecret,
+});
 
 const app = createApp(App);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
+
+app.config.productionTip = false;
 
 app.use(router);
 
